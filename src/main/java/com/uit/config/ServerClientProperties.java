4fa890d430818c404fc4cb6a6547102cfb426564
king <kingcrabnd@gmail.com>
2026-04-14 16:18:24 +0700

    add log info server client

--- src/main/java/com/uit/config/ServerClientProperties.java
+++ src/main/java/com/uit/config/ServerClientProperties.java
@@ -1,12 +1,15 @@
 package com.uit.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Data
 @Component
 @ConfigurationProperties(prefix = "server.client")
@@ -24,5 +27,17 @@ public class ServerClientProperties {
     @Data
     public static class Podcast {
         private String url;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (bive != null && bive.getPack() != null) {
+            log.info("Server client Bive url loaded: {}", bive.getUrl());
+            log.info("Server client Bive packages loaded: {}", bive.getPack());
+        }if (podcast != null) {
+            log.info("Server client Podcast url loaded: {}", podcast.getUrl());
+        }else {
+            log.warn("Bive packages is empty or not configured!");
+        }
     }
 }
